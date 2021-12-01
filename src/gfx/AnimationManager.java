50d1b6cb46627f858db34d98cf382025e367a0a6
Jonathan <Jonathanm.rosario@gmail.com>
2021-12-01 13:51:59 -0500

    Added animation manager class

---
+++ src/gfx/AnimationManager.java
@@ -0,0 +1,11 @@
+package gfx;
+import java.awt.image.BufferedImage;
+
+public class AnimationManager {
+	private SpriteSet spriteSet;
+	
+	private BufferedImage currentAnimationSheet;
+	private int currentFrameTime;
+	private int updatesPerFrame;
+	private int frameIndex;
+}
