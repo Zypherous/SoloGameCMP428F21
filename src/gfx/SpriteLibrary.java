99028d489343b32e560141bb9008579bc45ce219
Jonathan <Jonathanm.rosario@gmail.com>
2021-12-01 19:04:30 -0500

    Added Game State

--- src/gfx/SpriteLibrary.java
+++ src/gfx/SpriteLibrary.java
@@ -1,5 +1,9 @@
 package gfx;
 
+import game.Game;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
@@ -10,13 +14,31 @@ public class SpriteLibrary {
     private final static String PATH_TO_UNITS = "/sprites/units";
 
     private Map<String, SpriteSheet> units;
+    private Map<String, Image> tiles;
 
     public SpriteLibrary() {
         units = new HashMap<>();
+        tiles = new HashMap<>();
         loadSpritesFromDisk();
     }
 
     private void loadSpritesFromDisk() {
+        loadUnits();
+        loadTiles();
+    }
+
+    private void loadTiles() {
+        BufferedImage image = new BufferedImage(Game.SPRITE_SIZE, Game.SPRITE_SIZE, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = image.createGraphics();
+
+        graphics.setColor(Color.red);
+        graphics.drawRect(0, 0, Game.SPRITE_SIZE, Game.SPRITE_SIZE);
+
+        graphics.dispose();
+        tiles.put("default", image);
+    }
+
+    private void loadUnits() {
         String[] folderNames = getFolderNames(PATH_TO_UNITS);
 
         for(String folderName: folderNames) {
@@ -45,8 +67,12 @@ public class SpriteLibrary {
         File file = new File(resource.getFile());
         return file.list((current, name) -> new File(current, name).isDirectory());
     }
-    
+
     public SpriteSheet getUnit(String name) {
-    	return units.get(name);
+        return units.get(name);
+    }
+
+    public Image getTile(String name) {
+        return tiles.get(name);
     }
 }
