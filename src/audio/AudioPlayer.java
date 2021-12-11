171aec2ffca20df8fe2979cc0a6e5fb833ba7d23
Zypherous <Jonathanm.rosario@gmail.com>
2021-12-11 05:06:47 -0500

    Added audio stream and file loader

--- src/audio/AudioPlayer.java
+++ src/audio/AudioPlayer.java
@@ -1,12 +1,28 @@
 package audio;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class AudioPlayer {
 	private List<AudioClip> audioClips;
 
     public AudioPlayer() {
         audioClips = new ArrayList<>();
+    }
+    
+    public Clip getClip(String fileName) {
+    	final URL soundFIle = Audio.class.getResource("/sounds/" + fileName);
+    	try(audioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile)){
+    		
+    	}
+    	catch(UnsupportedAudioFileException | IOException e) {
+    		System.out.println(e);
+    	}
     }
 }
